267cc6fc366f02ffbc11dd4f0143c90c052c3af2
fakihap <fakih.anugerah@gmail.com>
2023-04-27 10:50:43 +0700

    feat : initialize gradle build tools

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,9 +1,9 @@
-import GUI.*;
 import javax.swing.*;
+import GUI.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! GUI STARTED!");
         MainMenu mainMenu = new MainMenu();
         JFrame frame = new JFrame("ObjectEnjoyer");
         frame.setContentPane(mainMenu);
