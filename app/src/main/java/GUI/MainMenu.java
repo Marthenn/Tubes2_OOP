4b31cda384d58351df76f90f1fbc0018ceeed857
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-04-30 18:05:20 +0700

    refactor: use lambda expression for clock threading

--- app/src/main/java/GUI/MainMenu.java
+++ app/src/main/java/GUI/MainMenu.java
@@ -23,18 +23,16 @@ public class MainMenu extends JPanel {
         tabbedPane.addTab("Home", new Home());
 
         // date clock threading
-        Thread clockThread = new Thread() {
-            public void run() {
-                try {
-                    while (true) {
-                        dateTime.setText(new java.util.Date().toString());
-                        sleep(1000);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        Thread clockThread = new Thread(() -> {
+            try {
+                while (true) {
+                    dateTime.setText(new java.util.Date().toString());
+                    Thread.sleep(1000);
                 }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        };
+        });
         clockThread.start();
     }
 
@@ -109,13 +107,12 @@ public class MainMenu extends JPanel {
 
         //======== this ========
         setPreferredSize(new Dimension(800, 600));
-        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax
-        . swing. border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing
-        . border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java .awt .
-        Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt. Color. red
-        ) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override
-        public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r" .equals (e .getPropertyName (
-        ) )) throw new RuntimeException( ); }} );
+        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border.
+        EmptyBorder( 0, 0, 0, 0) , "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e", javax. swing. border. TitledBorder. CENTER, javax. swing
+        . border. TitledBorder. BOTTOM, new java .awt .Font ("D\u0069al\u006fg" ,java .awt .Font .BOLD ,12 ),
+        java. awt. Color. red) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( )
+        { @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062or\u0064er" .equals (e .getPropertyName () ))
+        throw new RuntimeException( ); }} );
         setLayout(new BorderLayout());
 
         //======== menuBar ========
