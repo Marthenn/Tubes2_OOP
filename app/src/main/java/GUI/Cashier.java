7065cf9f6da72194cd816bbd96fa65890ea1957d
fakihap <fakih.anugerah@gmail.com>
2023-05-04 22:52:23 +0700

    chore : add naming clarity

--- app/src/main/java/GUI/Cashier.java
+++ app/src/main/java/GUI/Cashier.java
@@ -16,70 +16,68 @@ public class Cashier extends JPanel {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - Fakih Anugerah Pratama
-        label1 = new JLabel();
-        scrollPane1 = new JScrollPane();
-        list3 = new JList();
-        textField1 = new JTextField();
-        button1 = new JButton();
-        tabbedPane1 = new JTabbedPane();
-        scrollPane2 = new JScrollPane();
-        list4 = new JList();
-        label4 = new JLabel();
-        label5 = new JLabel();
-        button2 = new JButton();
-        button3 = new JButton();
+        title = new JLabel();
+        browsePane = new JScrollPane();
+        browseList = new JList();
+        searchText = new JTextField();
+        searchButton = new JButton();
+        billTabPane = new JTabbedPane();
+        billDetailPane = new JScrollPane();
+        billItemList = new JList();
+        subtotalTitle = new JLabel();
+        subtotalAmount = new JLabel();
+        saveBill = new JButton();
+        printBill = new JButton();
 
         //======== this ========
-        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax
-        . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e" , javax. swing
-        .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .
-        Font ( "Dialo\u0067", java .awt . Font. BOLD ,12 ) ,java . awt. Color .red
-        ) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override
-        public void propertyChange (java . beans. PropertyChangeEvent e) { if( "borde\u0072" .equals ( e. getPropertyName (
-        ) ) )throw new RuntimeException( ) ;} } );
+        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder(
+        0, 0, 0, 0) , "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e", javax. swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder
+        . BOTTOM, new java .awt .Font ("D\u0069al\u006fg" ,java .awt .Font .BOLD ,12 ), java. awt. Color.
+        red) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .
+        beans .PropertyChangeEvent e) {if ("\u0062or\u0064er" .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
 
-        //---- label1 ----
-        label1.setText("Cashier");
-        label1.setFont(label1.getFont().deriveFont(label1.getFont().getSize() + 4f));
+        //---- title ----
+        title.setText("Cashier");
+        title.setFont(title.getFont().deriveFont(title.getFont().getSize() + 4f));
 
-        //======== scrollPane1 ========
+        //======== browsePane ========
         {
 
-            //---- list3 ----
-            list3.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-            scrollPane1.setViewportView(list3);
+            //---- browseList ----
+            browseList.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+            browsePane.setViewportView(browseList);
         }
 
-        //---- textField1 ----
-        textField1.setMargin(new Insets(0, 0, 0, 0));
+        //---- searchText ----
+        searchText.setMargin(new Insets(0, 0, 0, 0));
 
-        //---- button1 ----
-        button1.setText("search");
+        //---- searchButton ----
+        searchButton.setText("search");
 
-        //======== tabbedPane1 ========
+        //======== billTabPane ========
         {
 
-            //======== scrollPane2 ========
+            //======== billDetailPane ========
             {
 
-                //---- list4 ----
-                list4.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-                scrollPane2.setViewportView(list4);
+                //---- billItemList ----
+                billItemList.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+                billDetailPane.setViewportView(billItemList);
             }
-            tabbedPane1.addTab("text", scrollPane2);
+            billTabPane.addTab("text", billDetailPane);
         }
 
-        //---- label4 ----
-        label4.setText("subtotal");
+        //---- subtotalTitle ----
+        subtotalTitle.setText("subtotal");
 
-        //---- label5 ----
-        label5.setText("0");
+        //---- subtotalAmount ----
+        subtotalAmount.setText("0");
 
-        //---- button2 ----
-        button2.setText("search");
+        //---- saveBill ----
+        saveBill.setText("save");
 
-        //---- button3 ----
-        button3.setText("search");
+        //---- printBill ----
+        printBill.setText("print");
 
         GroupLayout layout = new GroupLayout(this);
         setLayout(layout);
@@ -89,54 +87,54 @@ public class Cashier extends JPanel {
                     .addGap(18, 18, 18)
                     .addGroup(layout.createParallelGroup()
                         .addGroup(layout.createSequentialGroup()
-                            .addComponent(label1, GroupLayout.PREFERRED_SIZE, 296, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(title, GroupLayout.PREFERRED_SIZE, 296, GroupLayout.PREFERRED_SIZE)
                             .addGap(0, 0, Short.MAX_VALUE))
                         .addGroup(layout.createSequentialGroup()
                             .addGroup(layout.createParallelGroup()
                                 .addGroup(layout.createSequentialGroup()
-                                    .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 433, GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(browsePane, GroupLayout.PREFERRED_SIZE, 433, GroupLayout.PREFERRED_SIZE)
                                     .addGap(0, 0, Short.MAX_VALUE))
                                 .addGroup(layout.createSequentialGroup()
-                                    .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 369, GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(searchText, GroupLayout.PREFERRED_SIZE, 369, GroupLayout.PREFERRED_SIZE)
                                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(button1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                                    .addComponent(searchButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                             .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)))
                     .addGroup(layout.createParallelGroup()
-                        .addComponent(tabbedPane1, GroupLayout.PREFERRED_SIZE, 290, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(billTabPane, GroupLayout.PREFERRED_SIZE, 290, GroupLayout.PREFERRED_SIZE)
                         .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
                             .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(button2, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(saveBill, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(button3, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE))
+                                .addComponent(printBill, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(label4, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(subtotalTitle, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(label5, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                                .addComponent(subtotalAmount, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                     .addGap(33, 33, 33))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup()
                 .addGroup(layout.createSequentialGroup()
                     .addContainerGap()
-                    .addComponent(label1, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(title, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
                     .addGap(18, 18, 18)
                     .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                        .addComponent(textField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                        .addComponent(button1))
+                        .addComponent(searchText, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(searchButton))
                     .addGap(18, 18, 18)
-                    .addComponent(scrollPane1, GroupLayout.DEFAULT_SIZE, 355, Short.MAX_VALUE)
+                    .addComponent(browsePane, GroupLayout.DEFAULT_SIZE, 355, Short.MAX_VALUE)
                     .addContainerGap(136, Short.MAX_VALUE))
                 .addGroup(layout.createSequentialGroup()
                     .addGap(86, 86, 86)
-                    .addComponent(tabbedPane1, GroupLayout.PREFERRED_SIZE, 320, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(billTabPane, GroupLayout.PREFERRED_SIZE, 320, GroupLayout.PREFERRED_SIZE)
                     .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                     .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                        .addComponent(button3)
-                        .addComponent(button2))
+                        .addComponent(printBill)
+                        .addComponent(saveBill))
                     .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                     .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                        .addComponent(label4)
-                        .addComponent(label5))
+                        .addComponent(subtotalTitle)
+                        .addComponent(subtotalAmount))
                     .addGap(0, 124, Short.MAX_VALUE))
         );
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
@@ -144,17 +142,17 @@ public class Cashier extends JPanel {
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     // Generated using JFormDesigner Evaluation license - Fakih Anugerah Pratama
-    private JLabel label1;
-    private JScrollPane scrollPane1;
-    private JList list3;
-    private JTextField textField1;
-    private JButton button1;
-    private JTabbedPane tabbedPane1;
-    private JScrollPane scrollPane2;
-    private JList list4;
-    private JLabel label4;
-    private JLabel label5;
-    private JButton button2;
-    private JButton button3;
+    private JLabel title;
+    private JScrollPane browsePane;
+    private JList browseList;
+    private JTextField searchText;
+    private JButton searchButton;
+    private JTabbedPane billTabPane;
+    private JScrollPane billDetailPane;
+    private JList billItemList;
+    private JLabel subtotalTitle;
+    private JLabel subtotalAmount;
+    private JButton saveBill;
+    private JButton printBill;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
