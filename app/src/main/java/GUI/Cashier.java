cf2c431c817e86258e75d63634ad86f685c03fcc
fakihap <fakih.anugerah@gmail.com>
2023-05-04 22:49:06 +0700

    feat : cashier basic layout

--- app/src/main/java/GUI/Cashier.java
+++ app/src/main/java/GUI/Cashier.java
@@ -6,10 +6,8 @@ package GUI;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.TableModel;
 
-/**
- * @author Marthen
- */
 public class Cashier extends JPanel {
     public Cashier() {
         initComponents();
@@ -17,20 +15,146 @@ public class Cashier extends JPanel {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
-        // Generated using JFormDesigner Evaluation license - Bintang Dwi Marthen
+        // Generated using JFormDesigner Evaluation license - Fakih Anugerah Pratama
+        label1 = new JLabel();
+        scrollPane1 = new JScrollPane();
+        list3 = new JList();
+        textField1 = new JTextField();
+        button1 = new JButton();
+        tabbedPane1 = new JTabbedPane();
+        scrollPane2 = new JScrollPane();
+        list4 = new JList();
+        label4 = new JLabel();
+        label5 = new JLabel();
+        button2 = new JButton();
+        button3 = new JButton();
 
         //======== this ========
-        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border
-        .EmptyBorder(0,0,0,0), "JFor\u006dDesi\u0067ner \u0045valu\u0061tion",javax.swing.border.TitledBorder.CENTER,javax
-        .swing.border.TitledBorder.BOTTOM,new java.awt.Font("Dia\u006cog",java.awt.Font.BOLD,
-        12),java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans
-        .PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("bord\u0065r".equals(e.
-        getPropertyName()))throw new RuntimeException();}});
-        setLayout(new BorderLayout());
+        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax
+        . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e" , javax. swing
+        .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .
+        Font ( "Dialo\u0067", java .awt . Font. BOLD ,12 ) ,java . awt. Color .red
+        ) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override
+        public void propertyChange (java . beans. PropertyChangeEvent e) { if( "borde\u0072" .equals ( e. getPropertyName (
+        ) ) )throw new RuntimeException( ) ;} } );
+
+        //---- label1 ----
+        label1.setText("Cashier");
+        label1.setFont(label1.getFont().deriveFont(label1.getFont().getSize() + 4f));
+
+        //======== scrollPane1 ========
+        {
+
+            //---- list3 ----
+            list3.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+            scrollPane1.setViewportView(list3);
+        }
+
+        //---- textField1 ----
+        textField1.setMargin(new Insets(0, 0, 0, 0));
+
+        //---- button1 ----
+        button1.setText("search");
+
+        //======== tabbedPane1 ========
+        {
+
+            //======== scrollPane2 ========
+            {
+
+                //---- list4 ----
+                list4.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+                scrollPane2.setViewportView(list4);
+            }
+            tabbedPane1.addTab("text", scrollPane2);
+        }
+
+        //---- label4 ----
+        label4.setText("subtotal");
+
+        //---- label5 ----
+        label5.setText("0");
+
+        //---- button2 ----
+        button2.setText("search");
+
+        //---- button3 ----
+        button3.setText("search");
+
+        GroupLayout layout = new GroupLayout(this);
+        setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup()
+                .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addGap(18, 18, 18)
+                    .addGroup(layout.createParallelGroup()
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(label1, GroupLayout.PREFERRED_SIZE, 296, GroupLayout.PREFERRED_SIZE)
+                            .addGap(0, 0, Short.MAX_VALUE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup()
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 433, GroupLayout.PREFERRED_SIZE)
+                                    .addGap(0, 0, Short.MAX_VALUE))
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 369, GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(button1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)))
+                    .addGroup(layout.createParallelGroup()
+                        .addComponent(tabbedPane1, GroupLayout.PREFERRED_SIZE, 290, GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(button2, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(button3, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(label4, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(label5, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                    .addGap(33, 33, 33))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup()
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(label1, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
+                    .addGap(18, 18, 18)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(textField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(button1))
+                    .addGap(18, 18, 18)
+                    .addComponent(scrollPane1, GroupLayout.DEFAULT_SIZE, 355, Short.MAX_VALUE)
+                    .addContainerGap(136, Short.MAX_VALUE))
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(86, 86, 86)
+                    .addComponent(tabbedPane1, GroupLayout.PREFERRED_SIZE, 320, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(button3)
+                        .addComponent(button2))
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(label4)
+                        .addComponent(label5))
+                    .addGap(0, 124, Short.MAX_VALUE))
+        );
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    // Generated using JFormDesigner Evaluation license - Bintang Dwi Marthen
+    // Generated using JFormDesigner Evaluation license - Fakih Anugerah Pratama
+    private JLabel label1;
+    private JScrollPane scrollPane1;
+    private JList list3;
+    private JTextField textField1;
+    private JButton button1;
+    private JTabbedPane tabbedPane1;
+    private JScrollPane scrollPane2;
+    private JList list4;
+    private JLabel label4;
+    private JLabel label5;
+    private JButton button2;
+    private JButton button3;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
