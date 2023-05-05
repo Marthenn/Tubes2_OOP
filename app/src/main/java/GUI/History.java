d24b39931f51af6b933419e8843927e052d831f7
fakihap <fakih.anugerah@gmail.com>
2023-05-05 08:03:34 +0700

    feat : history styling

--- app/src/main/java/GUI/History.java
+++ app/src/main/java/GUI/History.java
@@ -18,63 +18,61 @@ public class History extends JPanel {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - Fakih Anugerah Pratama
-        label1 = new JLabel();
-        label5 = new JLabel();
-        label2 = new JLabel();
-        label6 = new JLabel();
-        textField1 = new JTextField();
-        scrollPane1 = new JScrollPane();
-        table1 = new JTable();
+        NameLabel = new JLabel();
+        Name = new JLabel();
+        MembershipLabel = new JLabel();
+        Membership = new JLabel();
+        SearchBox = new JTextField();
+        HistoryTablePane = new JScrollPane();
+        HistoryTable = new JTable();
 
         //======== this ========
-        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax
-        . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e" , javax. swing
-        .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .
-        Font ( "D\u0069al\u006fg", java .awt . Font. BOLD ,12 ) ,java . awt. Color .red
-        ) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override
-        public void propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062or\u0064er" .equals ( e. getPropertyName (
-        ) ) )throw new RuntimeException( ) ;} } );
+        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(
+        0,0,0,0), "JF\u006frmDes\u0069gner \u0045valua\u0074ion",javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder
+        .BOTTOM,new java.awt.Font("D\u0069alog",java.awt.Font.BOLD,12),java.awt.Color.
+        red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.
+        beans.PropertyChangeEvent e){if("\u0062order".equals(e.getPropertyName()))throw new RuntimeException();}});
         setLayout(new GridBagLayout());
         ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0, 0, 0, 0};
         ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0, 0};
-        ((GridBagLayout)getLayout()).columnWeights = new double[] {1.0, 0.0, 0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).columnWeights = new double[] {0.7, 1.0, 1.0, 1.0, 1.0E-4};
         ((GridBagLayout)getLayout()).rowWeights = new double[] {1.0, 0.0, 0.0, 0.0, 1.0E-4};
 
-        //---- label1 ----
-        label1.setText("Name");
-        add(label1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+        //---- NameLabel ----
+        NameLabel.setText("Name");
+        add(NameLabel, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 5, 5), 0, 0));
 
-        //---- label5 ----
-        label5.setText("DefaultName");
-        add(label5, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+        //---- Name ----
+        Name.setText("DefaultName");
+        add(Name, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 5, 5), 0, 0));
 
-        //---- label2 ----
-        label2.setText("Membership Status");
-        add(label2, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+        //---- MembershipLabel ----
+        MembershipLabel.setText("Membership Status");
+        add(MembershipLabel, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 5, 5), 0, 0));
 
-        //---- label6 ----
-        label6.setText("DefaultMembership");
-        add(label6, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
+        //---- Membership ----
+        Membership.setText("DefaultMembership");
+        add(Membership, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 5, 5), 0, 0));
 
-        //---- textField1 ----
-        textField1.setText("Search by");
-        add(textField1, new GridBagConstraints(0, 2, 2, 1, 0.0, 0.0,
+        //---- SearchBox ----
+        SearchBox.setText("Search by");
+        add(SearchBox, new GridBagConstraints(0, 2, 2, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 5, 5), 0, 0));
 
-        //======== scrollPane1 ========
+        //======== HistoryTablePane ========
         {
-            scrollPane1.setViewportView(table1);
+            HistoryTablePane.setViewportView(HistoryTable);
         }
-        add(scrollPane1, new GridBagConstraints(0, 3, 4, 1, 0.0, 0.0,
+        add(HistoryTablePane, new GridBagConstraints(0, 3, 4, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 0, 0), 0, 0));
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
@@ -82,12 +80,12 @@ public class History extends JPanel {
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     // Generated using JFormDesigner Evaluation license - Fakih Anugerah Pratama
-    private JLabel label1;
-    private JLabel label5;
-    private JLabel label2;
-    private JLabel label6;
-    private JTextField textField1;
-    private JScrollPane scrollPane1;
-    private JTable table1;
+    private JLabel NameLabel;
+    private JLabel Name;
+    private JLabel MembershipLabel;
+    private JLabel Membership;
+    private JTextField SearchBox;
+    private JScrollPane HistoryTablePane;
+    private JTable HistoryTable;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
