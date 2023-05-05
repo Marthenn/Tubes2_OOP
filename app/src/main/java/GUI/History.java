d1a7161d9aca7c483d1b3e7a579112b8f358d5ec
fakihap <fakih.anugerah@gmail.com>
2023-05-05 09:20:58 +0700

    feat : change by ID combobox implementation

--- app/src/main/java/GUI/History.java
+++ app/src/main/java/GUI/History.java
@@ -4,10 +4,20 @@
 
 package GUI;
 
+import Core.Customer.MembershipState.MembershipStateName;
+import Core.Customer.PremiumCustomer;
 import Core.DataStore.DataStore;
 import Core.Customer.Customer;
+import Core.DataStore.Exception.CustomerNotExistException;
+import Core.DataStore.Exception.PromotedCustomerAlreadyExist;
+import Core.DataStore.StorerData.Exception.SearchedItemNotExist;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.swing.*;
 
 /**
@@ -15,8 +25,25 @@ import javax.swing.*;
  */
 public class History extends JPanel {
     DataStore dataStore;
+    Customer selectedCustomer;
     public History() {
         this.dataStore = DataStore.getInstance();
+
+        //DEBUG POPULATING//
+        dataStore.createNewCustomer();
+        dataStore.createNewCustomer();
+        dataStore.createNewCustomer();
+        try {
+//
+            dataStore.promoteCustomer(1, "Bembi", "10", "11", MembershipStateName.MEMBER);
+
+        } catch (CustomerNotExistException e) {
+            System.out.println("a");
+        } catch (PromotedCustomerAlreadyExist e) {
+            System.out.println("b");
+        }
+        // HAPUS KALO GA DIPAKE //
+
 
         initComponents();
     }
@@ -24,14 +51,18 @@ public class History extends JPanel {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - Fakih Anugerah Pratama
-        DefaultComboBoxModel memberListModel = new DefaultComboBoxModel();
+        DefaultComboBoxModel customerListModel = new DefaultComboBoxModel();
 
-        for (Customer customer : dataStore.getCustomers()) {
-            memberListModel.addElement(customer.getID());
+        List<? extends Customer> listOfAllCustomers = Stream.of(dataStore.getCustomers(), dataStore.getPremiumCustomers())
+                                                        .flatMap(x -> x.stream())
+                                                        .collect(Collectors.toList());
+
+        for (Customer customer : listOfAllCustomers) {
+            customerListModel.addElement(customer.getID());
         }
 
         NameLabel = new JLabel();
-        Name = new JComboBox(memberListModel);
+        Name = new JComboBox(customerListModel);
         MembershipLabel = new JLabel();
         Membership = new JLabel();
         SearchBox = new JTextField();
@@ -80,6 +111,22 @@ public class History extends JPanel {
         add(HistoryTablePane, new GridBagConstraints(0, 3, 4, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 0, 0), 0, 0));
+
+
+        Name.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+
+                updateCustomerData((int) Name.getSelectedItem());
+
+            }
+        });
+
+
+        // SET DEFAULT VALUE
+        if (listOfAllCustomers.size() != 0) {
+            updateCustomerData((int)customerListModel.getSelectedItem());
+        }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
@@ -93,4 +140,31 @@ public class History extends JPanel {
     private JScrollPane HistoryTablePane;
     private JTable HistoryTable;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+
+    Boolean isMember(Customer customer) {
+        return customer.getClass() == PremiumCustomer.class;
+    }
+
+    void updateCustomerData(int customerID) {
+        // TODO : gabungin
+        try {
+            selectedCustomer = dataStore.getCustomerWithID(customerID);
+        } catch (SearchedItemNotExist e) {
+
+        }
+
+        try {
+            selectedCustomer = dataStore.getPremiumCustomerWithID(customerID);
+        } catch (SearchedItemNotExist e) {
+
+        }
+
+        if (!isMember(selectedCustomer)) {
+            Membership.setText("Customer");
+        } else {
+            PremiumCustomer _premiumCustomer = (PremiumCustomer) selectedCustomer;
+            Membership.setText(_premiumCustomer.getStatus().name());
+        }
+
+    }
 }
