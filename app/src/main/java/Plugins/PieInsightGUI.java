6d9906be9e588bab05c3f1f4d986ff841f8d54d9
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-05-07 03:04:46 +0700

    feat: calculate the solditems data

--- app/src/main/java/Plugins/PieInsightGUI.java
+++ app/src/main/java/Plugins/PieInsightGUI.java
@@ -5,14 +5,56 @@
 package Plugins;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.*;
+
+import Core.DataStore.DataStore;
+import Core.Item.Bill.FixedBill.*;
+import Core.Customer.Customer;
+import Core.Customer.PremiumCustomer;
+import Core.Item.QuantifiableItem;
 
 /**
  * @author Marthen
  */
 public class PieInsightGUI extends JPanel {
+
+    private DataStore ds = DataStore.getInstance();
+
     public PieInsightGUI() {
         initComponents();
+        Thread updateData = new Thread (() -> {
+        });
+    }
+
+    // return dari method ini adalah map id dengan jumlah item yang terjual
+    private Map<Integer, Integer> getSoldItems(){
+        Map<Integer, Integer> soldItems = new HashMap<>();
+        for (Customer cs : ds.getCustomers()){
+            for (FixedBill fb : cs.getHistory()){
+                for (QuantifiableItem qi : fb.getItems()) {
+                    if (soldItems.containsKey(qi.getID())) {
+                        soldItems.put(qi.getID(), soldItems.get(qi.getID()) + qi.getQuantity());
+                    } else {
+                        soldItems.put(qi.getID(), qi.getQuantity());
+                    }
+                }
+            }
+        }
+        for (PremiumCustomer pc : ds.getPremiumCustomers()){
+            for (FixedBill fb : pc.getHistory()){
+                for (QuantifiableItem qi : fb.getItems()) {
+                    if (soldItems.containsKey(qi.getID())) {
+                        soldItems.put(qi.getID(), soldItems.get(qi.getID()) + qi.getQuantity());
+                    } else {
+                        soldItems.put(qi.getID(), qi.getQuantity());
+                    }
+                }
+            }
+        }
+        return soldItems;
     }
 
     private void initComponents() {
@@ -21,17 +63,17 @@ public class PieInsightGUI extends JPanel {
         label1 = new JLabel();
 
         //======== this ========
-        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax.
-        swing. border. EmptyBorder( 0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax. swing. border
-        . TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog"
-        ,java .awt .Font .BOLD ,12 ), java. awt. Color. red) , getBorder
-        ( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java
-        .beans .PropertyChangeEvent e) {if ("bord\u0065r" .equals (e .getPropertyName () )) throw new RuntimeException
-        ( ); }} );
+        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax
+        .swing.border.EmptyBorder(0,0,0,0), "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn",javax.swing
+        .border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java.awt.
+        Font("Dia\u006cog",java.awt.Font.BOLD,12),java.awt.Color.red
+        ), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override
+        public void propertyChange(java.beans.PropertyChangeEvent e){if("\u0062ord\u0065r".equals(e.getPropertyName(
+        )))throw new RuntimeException();}});
         setLayout(null);
 
         //---- label1 ----
-        label1.setText("TEST PLUGIN PIECHART");
+        label1.setText("INI PIE CHARTNYA NANTI");
         add(label1);
         label1.setBounds(new Rectangle(new Point(345, 260), label1.getPreferredSize()));
 
