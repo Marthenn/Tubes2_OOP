109ba6a0000df406e7870462d1b99c190ac29c68
althaafka <althaafka004@gmail.com>
2023-05-06 17:01:29 +0700

    add filecontroller

---
+++ app/src/main/java/Core/FileController/JsonController.java
@@ -0,0 +1,40 @@
+package Core.FileController;
+
+import Core.DataStore.DataStore;
+import Core.DataStore.StorerData.StorerData;
+import Core.Item.Bill.Image.ImageWithID;
+import Core.Item.QuantifiableItem;
+import Core.Settings;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+
+public class JsonController implements FileController{
+//    @Override
+//    public void saveItem() {
+//
+//    }
+//
+//    @Override
+//    public void saveBill() {
+//
+//    }
+    public JsonController(){
+    }
+
+    @Override
+    public void saveImage(StorerData<ImageWithID> images) throws IOException {
+        String json = new ObjectMapper().writeValueAsString(images);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(Paths.get(Settings.getInstance().getDirectoryPath()+"\\image.json").toFile(), json);
+    }
+
+    public void saveItem(StorerData<QuantifiableItem> item) throws IOException {
+        String json = new ObjectMapper().writeValueAsString(item);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(Paths.get(Settings.getInstance().getDirectoryPath()+"\\item.json").toFile(), json);
+    }
+}
