d4ce40a5467a1488ce4bc4c7353d8dc7a009c22c
Putinabillaa <aidiraputiabil@gmail.com>
2023-05-07 17:11:04 +0700

    fixxxx

--- app/src/main/java/Core/Settings/Settings.java
+++ app/src/main/java/Core/Settings/Settings.java
@@ -1,3 +1,5 @@
+
+
 package Core.Settings;
 
 import Plugins.Plugin;
@@ -7,13 +9,13 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static Plugins.Helper.loadClasses;
 
@@ -22,7 +24,7 @@ public class Settings {
     // hal ini dilatarbelakangi oleh datanya yang berasal dari GUI
     private static Settings instance = null;
     @Setter @Getter(AccessLevel.PUBLIC)
-    private String path;
+    private String path = null;
 
     @Getter(AccessLevel.PUBLIC)
     @Setter
@@ -82,12 +84,24 @@ public class Settings {
         mapper.writeValue(Paths.get("jsonConfig/filetype_config.json").toFile(), this.saveFileType);
     }
     public void loadPath() throws IOException {
-        String json = new String(Files.readAllBytes(Paths.get("jsonConfig/path_config.json")));
-        this.path =  new ObjectMapper().readValue(json, String.class);
+        String json = null;
+        try {
+            json = new String(Files.readAllBytes(Paths.get("jsonConfig/path_config.json")));
+            this.path =  new ObjectMapper().readValue(json, String.class);
+        } catch (IOException e) {
+            new FileWriter("jsonConfig/path_config.json");
+        }
     }
     public void loadFileType() throws IOException {
-        String json = new String(Files.readAllBytes(Paths.get("jsonConfig/filetype_config.json")));
-        this.saveFileType = new ObjectMapper().readValue(json, SaveFileType.class);
+        String json = null;
+        try {
+            json = new String(Files.readAllBytes(Paths.get("jsonConfig/filetype_config.json")));
+            this.saveFileType.name =  new ObjectMapper().readValue(json, String.class);
+        } catch (IOException e) {
+            String filetype = "JSON";
+            new ObjectMapper().writeValue(new File("jsonConfig/filetype_config.json"), filetype);
+        }
     }
 }
+
 
