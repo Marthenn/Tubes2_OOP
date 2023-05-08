5d5dad405dd0d2f56c827ab7723eecee09fa63ae
fakihap <fakih.anugerah@gmail.com>
2023-05-08 08:37:12 +0700

    feat : handling paywithpoit, unsufficient stock, premium status

--- app/src/main/java/GUI/CashierCheckout.java
+++ app/src/main/java/GUI/CashierCheckout.java
@@ -5,6 +5,7 @@
 package GUI;
 
 import Core.Customer.Customer;
+import Core.Customer.PremiumCustomer;
 import Core.DataStore.DataStore;
 import Core.DataStore.StorerData.StorerDataListener;
 import Core.IDAble.IDAbleListener;
@@ -12,21 +13,16 @@ import Core.Printer.FixedBillPrinter;
 import Core.Item.Bill.Bill;
 import Core.Item.Bill.FixedBill.FixedBillModifier.FixedBillModifier;
 import Core.Item.QuantifiableItem;
-import lombok.Data;
 import lombok.SneakyThrows;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Base64;
 
 /**
  * @author Fakih A
@@ -37,6 +33,7 @@ public class CashierCheckout extends JPanel implements StorerDataListener, IDAbl
     Bill billToBeCheckedOut; // add listeners
     DefaultTableModel tabelDetailModel = new DefaultTableModel();
     ArrayList<FixedBillModifier> fixedBillModifiers = new ArrayList<>();
+    Boolean adaStokKosong = false;
     public CashierCheckout(JTabbedPane parentTabbedPane, Cashier parentCashier, Bill billToBeCheckedOut) {
         this.parentTabbedPane = parentTabbedPane;
         this.parentCashier = parentCashier;
@@ -63,7 +60,8 @@ public class CashierCheckout extends JPanel implements StorerDataListener, IDAbl
         TabelDetail = new JTable();
         LabelTotalPembelian = new JLabel();
         NilaiTotalPembelian = new JLabel();
-        Checkout = new JButton();
+        PayButton = new JButton();
+        PayWithPointButton = new JButton();
 
         TabelDetail.setModel(tabelDetailModel);
 
@@ -94,7 +92,7 @@ public class CashierCheckout extends JPanel implements StorerDataListener, IDAbl
         {
             DaftarPembelian.setViewportView(TabelPembelian);
         }
-        add(DaftarPembelian, new GridBagConstraints(3, 0, 2, 3, 0.0, 0.0,
+        add(DaftarPembelian, new GridBagConstraints(3, 0, 2, 2, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 5, 0), 0, 0));
 
@@ -117,45 +115,112 @@ public class CashierCheckout extends JPanel implements StorerDataListener, IDAbl
 
         //---- LabelTotalPembelian ----
         LabelTotalPembelian.setText("Total");
-        add(LabelTotalPembelian, new GridBagConstraints(3, 3, 1, 1, 0.0, 0.0,
+        add(LabelTotalPembelian, new GridBagConstraints(3, 2, 1, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 5, 5), 0, 0));
 
         //---- NilaiTotalPembelian ----
         updateNilaiTotalPembelian();
-        add(NilaiTotalPembelian, new GridBagConstraints(4, 3, 1, 1, 0.0, 0.0,
+        add(NilaiTotalPembelian, new GridBagConstraints(4, 2, 1, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 5, 0), 0, 0));
 
         //---- Checkout ----
-        Checkout.setText("Checkout");
-        add(Checkout, new GridBagConstraints(3, 4, 2, 1, 0.0, 0.0,
+        PayButton.setText("Pay");
+        add(PayButton, new GridBagConstraints(3, 3, 2, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 0, 0), 0, 0));
 
-        Checkout.addActionListener(new ActionListener() {// MAY RESULT IN DEACTIVATED USER
+        //---- Checkout ----
+        PayWithPointButton.setText("Pay with Points");
+        add(PayWithPointButton, new GridBagConstraints(3, 4, 2, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 0, 0), 0, 0));
+
+        PayButton.addActionListener(new ActionListener() {// MAY RESULT IN DEACTIVATED USER
             @SneakyThrows
             @Override
             public void actionPerformed(ActionEvent e) {
 //                System.out.println(NamaCustomer.getSelectedCustomerID());
                 // function finish checkout
+                if (adaStokKosong) {
+                    JOptionPane.showMessageDialog(null, "Ada barang yang tidak tersedia", "Payment", JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
 
-                Customer billOwner = NamaCustomer.getSelectedCustomerID() == -1
-                        ?
-                        DataStore.getInstance().createNewCustomer()                                         // soon-to-be-customer
-                        :
-                        DataStore.getInstance().getPremiumCustomerWithID(NamaCustomer.getSelectedCustomerID()      // premium customer
-                        );
+                Customer billOwner;
+                boolean isPremium =  NamaCustomer.getSelectedCustomerID() != -1;
+
+                if (isPremium) {
+                    billOwner = DataStore.getInstance().getPremiumCustomerWithID(NamaCustomer.getSelectedCustomerID())    ;  // premium customer
+                } else {
+                    billOwner = DataStore.getInstance().createNewCustomer()      ;                                   // soon-to-be-customer
+                }
 
                 billToBeCheckedOut.setOwner(billOwner);
                 billOwner.assignBill(billToBeCheckedOut);
 
-//                System.out.println("Bill Id = " + billToBeCheckedOut.getID());
-//                System.out.println(DataStore.getInstance().getBillWithID(billToBeCheckedOut).getID());
-
                 // pay
                 // TODO : add modifiers if exists
                 billOwner.pay();
+
+                // pop up print bill
+                int custId = billOwner.getID();
+                int fixedbillidx = billOwner.getHistory().size() - 1; // print latest
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                fileChooser.setDialogTitle("Print Bill");
+                int userSelection = JOptionPane.showConfirmDialog(null, "Do you want to print the bill?", "Print Bill?", JOptionPane.YES_NO_OPTION);
+                if (userSelection == JOptionPane.YES_OPTION) {
+                    int result = fileChooser.showSaveDialog(null);
+                    if (result == JFileChooser.APPROVE_OPTION) {
+                        File fileToSave = fileChooser.getSelectedFile();
+                        FixedBillPrinter fixedBillPrinter = new FixedBillPrinter(fileToSave.getAbsolutePath()+ File.separator + "bill_"+custId+"_"+fixedbillidx+".pdf", custId,
+                                fixedbillidx);
+                        Thread printThread = new Thread(fixedBillPrinter::printFixedBill);
+                        printThread.start();
+                    }
+                } else {
+                    // Don't save file
+                }
+                // hide this page (TODO: destroy or recycle instead)
+                parentTabbedPane.setComponentAt(parentTabbedPane.getSelectedIndex(), parentCashier);
+                parentTabbedPane.repaint();
+
+                // close bill tab
+                parentCashier.removeCurrentBillTab();
+                removeAll();
+            }
+        });
+
+        PayWithPointButton.addActionListener(new ActionListener() {// MAY RESULT IN DEACTIVATED USER
+            @SneakyThrows
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (adaStokKosong) {
+                    JOptionPane.showMessageDialog(null, "Ada barang yang tidak tersedia", "Payment", JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
+
+                // function finish checkout
+                Customer billOwner;
+                boolean isPremium = NamaCustomer.getSelectedCustomerID() != -1;
+
+                // pay
+                // TODO : add modifiers if exists
+                if (isPremium) {
+                    billOwner = DataStore.getInstance().createNewCustomer();                                         // soon-to-be-customer
+                    PremiumCustomer pCustomer = DataStore.getInstance().getPremiumCustomerWithID(billOwner.getID());
+
+                    billToBeCheckedOut.setOwner(billOwner);
+                    billOwner.assignBill(billToBeCheckedOut);
+
+                    pCustomer.payWithPoint();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Bukan Premium Customer", "Payment", JOptionPane.WARNING_MESSAGE);
+
+                    return;
+                }
 
                 // pop up print bill
                 int custId = billOwner.getID();
@@ -201,6 +266,7 @@ public class CashierCheckout extends JPanel implements StorerDataListener, IDAbl
 
     @SneakyThrows // TODO : check ketika item dihapus
     void updateTabelDetailModel() {
+        adaStokKosong = false;
         tabelDetailModel.setRowCount(0);
 
         int i = 1;
@@ -212,6 +278,10 @@ public class CashierCheckout extends JPanel implements StorerDataListener, IDAbl
                     Double.toString(qItem.getPrice()),
                     Integer.toString(DataStore.getInstance().getItemWithID(qItem.getID()).getQuantity() - qItem.getQuantity())
             });
+
+            if (DataStore.getInstance().getItemWithID(qItem.getID()).getQuantity() - qItem.getQuantity() < 0) {
+                adaStokKosong = true;
+            }
         }
     }
 
@@ -235,7 +305,8 @@ public class CashierCheckout extends JPanel implements StorerDataListener, IDAbl
     private JTable TabelDetail;
     private JLabel LabelTotalPembelian;
     private JLabel NilaiTotalPembelian;
-    private JButton Checkout;
+    private JButton PayButton;
+    private JButton PayWithPointButton;
 
     @Override
     public void onStorerDataChange(String storerName) {
